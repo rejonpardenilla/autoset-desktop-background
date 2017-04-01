0f11bc9f532e112f91720d2082dd951328b9b956
Daniel Rejon <rejonpardenilla@gmail.com>
2017-04-01 12:52:05 -0600

    cron now executes every 4 hours

--- cron.py
+++ cron.py
@@ -12,5 +12,5 @@ username = getpass.getuser()
 # TODO: Obtain the hour of the day diffetently
 cronjob = CronTab(user = username)
 job = cronjob.new(command = AUTOSET)
-job.setall('0 12 * * *')  # every day at 12:00 hrs 
+job.setall('0 */4 * * *')  # every 4 hours
 cronjob.write()
